301e4e01fc62cc9eed343f1215c76d3bdf07b7ba
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-05-25 06:28:03 +0100

    feat: implement event service

---
+++ src/modules/events/event.service.ts
@@ -0,0 +1,86 @@
+import Event from "./event.model";
+
+const createEvent = async (
+  payload: any
+) => {
+  const event = await Event.create(payload);
+
+  return event;
+};
+
+const getMyEvents = async (
+  ownerId: string
+) => {
+  const events = await Event.find({
+    ownerId,
+  }).sort({
+    createdAt: -1,
+  });
+
+  return events;
+};
+
+const getSingleEvent = async (
+  eventId: string,
+  ownerId: string
+) => {
+  const event = await Event.findOne({
+    _id: eventId,
+    ownerId,
+  });
+
+  if (!event) {
+    throw new Error("Event not found");
+  }
+
+  return event;
+};
+
+const updateEvent = async (
+  eventId: string,
+  ownerId: string,
+  payload: any
+) => {
+  const event =
+    await Event.findOneAndUpdate(
+      {
+        _id: eventId,
+        ownerId,
+      },
+      payload,
+      {
+        new: true,
+      }
+    );
+
+  if (!event) {
+    throw new Error("Event not found");
+  }
+
+  return event;
+};
+
+const deleteEvent = async (
+  eventId: string,
+  ownerId: string
+) => {
+  const event =
+    await Event.findOneAndDelete({
+      _id: eventId,
+      ownerId,
+    });
+
+  if (!event) {
+    throw new Error("Event not found");
+  }
+
+  return event;
+};
+
+export default {
+  createEvent,
+  getMyEvents,
+  getSingleEvent,
+  updateEvent,
+  deleteEvent,
+};
