3def57a1728d1ba84d4ce9ed2b0462ebc90910cf
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-05-25 07:39:12 +0100

    feat: implement ticket controller

---
+++ src/modules/tickets/ticket.controller.ts
@@ -0,0 +1,139 @@
+import { Response } from "express";
+
+import ticketService from "./ticket.service";
+
+const createTicket = async (
+  req: any,
+  res: Response
+) => {
+  try {
+    const payload = {
+      ...req.body,
+
+      ownerId: req.user.userId,
+    };
+
+    const ticket =
+      await ticketService.createTicket(
+        payload
+      );
+
+    res.status(201).json({
+      success: true,
+      message:
+        "Ticket created successfully",
+      data: ticket,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const getMyTickets = async (
+  req: any,
+  res: Response
+) => {
+  try {
+    const tickets =
+      await ticketService.getMyTickets(
+        req.user.userId
+      );
+
+    res.status(200).json({
+      success: true,
+      data: tickets,
+    });
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const getSingleTicket = async (
+  req: any,
+  res: Response
+) => {
+  try {
+    const ticket =
+      await ticketService.getSingleTicket(
+        req.params.id,
+        req.user.userId
+      );
+
+    res.status(200).json({
+      success: true,
+      data: ticket,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const updateTicket = async (
+  req: any,
+  res: Response
+) => {
+  try {
+    const ticket =
+      await ticketService.updateTicket(
+        req.params.id,
+        req.user.userId,
+        req.body
+      );
+
+    res.status(200).json({
+      success: true,
+      message:
+        "Ticket updated successfully",
+      data: ticket,
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const deleteTicket = async (
+  req: any,
+  res: Response
+) => {
+  try {
+    await ticketService.deleteTicket(
+      req.params.id,
+      req.user.userId
+    );
+
+    res.status(200).json({
+      success: true,
+      message:
+        "Ticket deleted successfully",
+    });
+  } catch (error: any) {
+    res.status(404).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export default {
+  createTicket,
+
+  getMyTickets,
+
+  getSingleTicket,
+
+  updateTicket,
+
+  deleteTicket,
+};
