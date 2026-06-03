15b3f12c7c6136233c409c9194672bc23b9d9fb7
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-03 12:03:39 +0100

    feat: create transfer model

---
+++ src/modules/transfers/transfer.model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const transferSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+
+    ticketIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Ticket",
+      },
+    ],
+
+    recipientFirstName: {
+      type: String,
+      required: true,
+    },
+
+    recipientLastName: {
+      type: String,
+      required: true,
+    },
+
+    recipientEmail: String,
+
+    recipientPhone: String,
+
+    note: String,
+
+    status: {
+      type: String,
+      enum: [
+        "COMPLETED",
+        "FAILED"
+      ],
+      default: "COMPLETED",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transfer = mongoose.model(
+  "Transfer",
+  transferSchema
+);
+
+export default Transfer;
