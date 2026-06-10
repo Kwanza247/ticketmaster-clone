72ca2b9e2dcdbdb452a38d533f13af0dd2d0853e
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-10 14:22:15 +0100

    feat: validate transfer requests

--- src/modules/transfers/transfer.routes.ts
+++ src/modules/transfers/transfer.routes.ts
@@ -3,12 +3,15 @@ import express from "express";
 import authMiddleware from "../../middlewares/auth.middleware";
 
 import transferController from "./transfer.controller";
+import validate from "../../middlewares/validate.middleware";
+import { transferSchema } from "../../validations/transfer.validation";
 
 const router = express.Router();
 
 router.post(
   "/",
   authMiddleware,
+  validate(transferSchema),
   transferController.transferTickets
 );
 
