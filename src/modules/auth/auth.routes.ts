5df1376d91b409777e443fa6932aaf9f21e799b4
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-10 14:14:25 +0100

    feat: validate login requests

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import authController from "./auth.controller";
+import validate from "../../middlewares/validate.middleware";
+import { loginSchema } from "../../validations/auth.validation";
 
 const router = express.Router();
 
-router.post("/login", authController.login);
+router.post(
+  "/login",
+  validate(loginSchema),
+  authController.login
+);
 
 export default router;
