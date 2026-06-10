e8f1f7e77761c980a43e362eadc6d8069acca4ca
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-10 14:28:46 +0100

    feat: validate ticket update requests

--- src/modules/tickets/ticket.routes.ts
+++ src/modules/tickets/ticket.routes.ts
@@ -5,6 +5,7 @@ import ticketController from "./ticket.controller";
 import authMiddleware from "../../middlewares/auth.middleware";
 import validate from "../../middlewares/validate.middleware";
 import { createTicketSchema } from "../../validations/ticket.validation";
+import { updateTicketSchema } from "../../validations/ticket.validation";
 const router = express.Router();
 
 router.post(
@@ -12,6 +13,13 @@ router.post(
   authMiddleware,
   validate(createTicketSchema),
   ticketController.createTicket
+);
+
+router.patch(
+  "/:id",
+  authMiddleware,
+  validate(updateTicketSchema),
+  ticketController.updateTicket
 );
 
 router.get(
