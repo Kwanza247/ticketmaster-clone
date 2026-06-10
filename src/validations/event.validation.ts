c14e8419b268e9864516a9998c130989168283f8
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-10 13:14:38 +0100

    feat: add event validation schema

---
+++ src/validations/event.validation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const createEventSchema = z.object({
+  title: z.string().min(3),
+
+  date: z.string(),
+
+  time: z.string(),
+
+  venue: z.string(),
+
+  city: z.string(),
+
+  state: z.string(),
+});
