37893c3c88be0c6b14cdebf3ac4f0a20fd6178f7
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-11 21:57:34 +0100

    refactor: use async handler for user creation

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,35 +1,33 @@
 import { Request, Response } from "express";
 import userService from "./user.service";
+import asyncHandler from "../../utils/asyncHandler";
 
-const createUser = async (
-  req: Request,
-  res: Response
-) => {
-  try {
+const createUser = asyncHandler(
+  async (
+    req: Request,
+    res: Response
+  ) => {
     const {
       username,
       password,
       role,
     } = req.body;
 
-    const user = await userService.createUser(
-      username,
-      password,
-      role
-    );
+    const user =
+      await userService.createUser(
+        username,
+        password,
+        role
+      );
 
     res.status(201).json({
       success: true,
-      message: "User created successfully",
+      message:
+        "User created successfully",
       data: user,
     });
-  } catch (error: any) {
-    res.status(400).json({
-      success: false,
-      message: error.message,
-    });
   }
-};
+);
 
 export default {
   createUser,
