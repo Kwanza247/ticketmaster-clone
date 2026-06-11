189f85d9b1edd214538ada6ca925ca517536c6ef
Ibrahim Adegboye <Ibrahimadegboye60@gmail.com>
2026-06-11 21:55:58 +0100

    refactor: use async handler for authentication

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,30 +1,28 @@
 import { Request, Response } from "express";
 import authService from "./auth.service";
+import asyncHandler from "../../utils/asyncHandler";
+const login = asyncHandler(
+  async (
+    req: Request,
+    res: Response
+  ) => {
+    const { username, password } =
+      req.body;
 
-const login = async (
-  req: Request,
-  res: Response
-) => {
-  try {
-    const { username, password } = req.body;
-
-    const data = await authService.loginUser(
-      username,
-      password
-    );
+    const data =
+      await authService.loginUser(
+        username,
+        password
+      );
 
     res.status(200).json({
       success: true,
-      message: "Login successful",
+      message:
+        "Login successful",
       data,
     });
-  } catch (error: any) {
-    res.status(401).json({
-      success: false,
-      message: error.message,
-    });
   }
-};
+);
 
 export default {
   login,
